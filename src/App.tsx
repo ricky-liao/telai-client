73e972eb220a0865d4dbc708f65ee594039322e8
Ricky Liao <liaoric@uw.edu>
2024-01-20 18:22:25 -0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { io } from "socket.io-client";
 
 function App() {
+
+  const connect = () => {
+    const socket = io("http://localhost:9000");
+    
+  }
+
+  useEffect(() => {
+    connect();
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome client</h1>
     </div>
   );
 }
